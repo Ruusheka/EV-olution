5768f5cd192790d9db4e96b2b308e3ae3d9530d4
Ruusheka <ruushekas@gmail.com>
2025-08-31 18:13:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import Bg from "./components/Bg/Bg";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+
+const App = () => {
+  const heroData = [
+    { text1: "Dive into", text2: "what you love" },
+    { text1: "Indulge", text2: "your passions" },
+    { text1: "Give in to", text2: "your passions" },
+  ];
+
+  const [heroCount, setHeroCount] = useState(0);
+  const [playStatus, setPlayStatus] = useState(true); // auto-play by default
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setHeroCount((count) => (count === heroData.length - 1 ? 0 : count + 1));
+    }, 3000); // change every 5 seconds
+
+    return () => clearInterval(interval); // cleanup
+  }, []);
+
+  return (
+    <div>
+      <Bg playStatus={playStatus} heroCount={heroCount} />
+      <Navbar />
+      <Hero
+        setPlayStatus={setPlayStatus}
+        heroData={heroData[heroCount]}
+        heroCount={heroCount}
+        setHeroCount={setHeroCount}
+        playStatus={playStatus}
+      />
+    </div>
+  );
+};
+
+export default App;
